f03eade96c733eceb9cee1025516a04bed6c865d
Merkisha Wilkinson <merkisha94@gmail.com>
2022-07-07 10:19:34 -0400

    declared variables added div via Js, and appended to site. Adjusted css to include height of text area

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,54 @@
+// declare global variables
+var timeStarts = moment().startOf('day').add(6,'h');
+var totalHours = 12;
+var currentTime = moment().format('H');
 
+// present, future, and past;
+var timeTableElement;
+var presentState;
+
+
+//date and time display
 function displayDate() {
-    // today display
-    var today = moment().format("dddd, MMMM Do YYYY, h:mm a");
-    $('#currentDay').text(today);
+    var now = moment().format("dddd, MMMM Do YYYY, h:mm a");
+    $('#currentDay').text(now);
 }
-
-// Function calls
 displayDate();
 
 
+
+function colorCode() {
+
+  for (var hour = 0; hour < totalHours; hour++) { 
+      var currentHour = hour + 9;
+      
+      timeTableElement = timeStarts.add(1,'h').format('h:mm a');
+
+      // determine present, past & future 
+      if (currentTime == currentHour) {
+          presentState = 'present';
+      } else if (currentTime < currentHour) {
+          presentState = 'past';
+      } else {
+          presentState = 'future';
+      }
+
+      let div = 
+          `<div id="hour-${currentHour}" class="row time-block ${presentState}">
+              <div class="col-md-1 hour">${timeTableElement}</div>
+              <textarea class="col-md-10 description ${currentHour}"></textarea>
+              <button class="btn saveBtn col-md-1">
+                  <i class="fas fa-save"></i>
+              </button>
+          </div>`;
+
+      $(".container").append(div);
+
+  }
+
+  loadSchedule();
+}
+
+colorCode();
+$('.saveBtn').on('click', saveSchedule);
 
